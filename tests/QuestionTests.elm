dda08950121611772780f05af3ff37e4b80ba8a6
John Crane <john.crane@gmail.com>
2019-01-13 10:09:52 -0800

    adding category tests - wip

--- tests/QuestionTests.elm
+++ tests/QuestionTests.elm
@@ -2,23 +2,39 @@ module QuestionTests exposing (suite)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
-import List exposing (all, any, length)
+import List exposing (all, any, foldl, length, map, range)
 import List.Extra exposing (unique)
 import Question exposing (Category(..), Question, createQuestion)
-import Random exposing (initialSeed)
+import Random exposing (Generator, initialSeed, step)
 import Set exposing (Set, fromList, toList)
 import Test exposing (..)
 import Tuple exposing (first, second)
 
 
-isEmptyQuestion : Question -> Bool
-isEmptyQuestion q =
-    any ((==) True)
-        [ q.centerChoice.category == EmptyCategory
-        , q.neChoice.category == EmptyCategory
-        , q.nwChoice.category == EmptyCategory
-        , q.seChoice.category == EmptyCategory
-        , q.swChoice.category == EmptyCategory
+choiceCategories : Question -> List Category
+choiceCategories q =
+    let
+        unique list =
+            case list of
+                [] ->
+                    []
+
+                [ x ] ->
+                    [ x ]
+
+                x :: xs ->
+                    if List.member x xs then
+                        unique xs
+
+                    else
+                        x :: unique xs
+    in
+    unique
+        [ q.centerChoice.category
+        , q.neChoice.category
+        , q.nwChoice.category
+        , q.seChoice.category
+        , q.swChoice.category
         ]
 
 
@@ -33,9 +49,38 @@ choiceNames q =
         ]
 
 
+isEmptyQuestion : Question -> Bool
+isEmptyQuestion q =
+    any ((==) True)
+        [ q.centerChoice.category == EmptyCategory
+        , q.neChoice.category == EmptyCategory
+        , q.nwChoice.category == EmptyCategory
+        , q.seChoice.category == EmptyCategory
+        , q.swChoice.category == EmptyCategory
+        ]
+
+
+{-| stole this from Random.elm, don't know why I couldn't import it
+-}
+bool : Generator Bool
+bool =
+    Random.map ((==) 1) (Random.int 0 1)
+
+
+randomlyExcludeElements : Int -> List a -> List Bool
+randomlyExcludeElements seed list =
+    let
+        bools =
+            List.range seed (length list + seed)
+                |> map (\s -> initialSeed s)
+                |> map (\s -> first (step bool s))
+    in
+    bools
+
+
 suite : Test
 suite =
-    describe "Test Sort-of-Trivia"
+    describe "Test Question.createQuestion"
         [ fuzz int "Seeds should not repeat" <|
             \randomSeed ->
                 let
@@ -64,5 +109,5 @@ suite =
                         createQuestion (Random.initialSeed randSeed) [ USCapitals ]
                 in
                 Expect.equal 5 <| length <| toList <| choiceNames q
-        , todo "Need to test at it only returns correct categories"
+        , todo "Need to test that it only returns correct categories"
         ]
